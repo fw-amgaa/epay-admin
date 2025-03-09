83b5dfd64fcfaf8884e3331e429ba83e4ec309e4
Amgalanbayar Amgalanbaatar <amgalanbayar@zerotech.mn>
2025-03-09 18:02:26 +0800

    Fixed filter names

--- apps/epay-admin/app/transactions/_lib/queries.ts
+++ apps/epay-admin/app/transactions/_lib/queries.ts
@@ -1,71 +1,76 @@
-import "server-only";
-import { unstable_cache } from "@/lib/unstable-cache";
-import { GetTransactionsParams } from "./validations";
-import { auth } from "@/auth";
-import { TransactionListApiResponse } from "./types";
+import 'server-only';
+import { unstable_cache } from '@/lib/unstable-cache';
+import { GetTransactionsParams } from './validations';
+import { auth } from '@/auth';
+import { TransactionListApiResponse } from './types';
 
 export async function getTransactions(input: GetTransactionsParams): Promise<TransactionListApiResponse> {
-  const session = await auth();
+    const session = await auth();
 
-  return await unstable_cache(
-    async () => {
-      try {
-        // const offset = (input.page - 1) * input.perPage;
-        // const fromDate = input.from ? new Date(input.from) : undefined;
-        // const toDate = input.to ? new Date(input.to) : undefined;
-        // const advancedTable = input.flags.includes("advancedTable");
+    console.log('input', input);
 
-        // const advancedWhere = filterColumns({
-        //   table: tasks,
-        //   filters: input.filters,
-        //   joinOperator: input.joinOperator,
-        // });
+    return await unstable_cache(
+        async () => {
+            try {
+                // const offset = (input.page - 1) * input.perPage;
+                // const fromDate = input.from ? new Date(input.from) : undefined;
+                // const toDate = input.to ? new Date(input.to) : undefined;
+                // const advancedTable = input.flags.includes("advancedTable");
 
-        // const where = advancedTable
-        //   ? advancedWhere
-        //   : and(
-        //       input.title ? ilike(tasks.title, `%${input.title}%`) : undefined,
-        //       input.status.length > 0
-        //         ? inArray(tasks.status, input.status)
-        //         : undefined,
-        //       input.priority.length > 0
-        //         ? inArray(tasks.priority, input.priority)
-        //         : undefined,
-        //       fromDate ? gte(tasks.createdAt, fromDate) : undefined,
-        //       toDate ? lte(tasks.createdAt, toDate) : undefined,
-        //     );
+                // const advancedWhere = filterColumns({
+                //   table: tasks,
+                //   filters: input.filters,
+                //   joinOperator: input.joinOperator,
+                // });
 
-        // const orderBy =
-        //   input.sort.length > 0
-        //     ? input.sort.map((item) =>
-        //         item.desc ? desc(tasks[item.id]) : asc(tasks[item.id]),
-        //       )
-        //     : [asc(tasks.createdAt)];
+                // const where = advancedTable
+                //   ? advancedWhere
+                //   : and(
+                //       input.title ? ilike(tasks.title, `%${input.title}%`) : undefined,
+                //       input.status.length > 0
+                //         ? inArray(tasks.status, input.status)
+                //         : undefined,
+                //       input.priority.length > 0
+                //         ? inArray(tasks.priority, input.priority)
+                //         : undefined,
+                //       fromDate ? gte(tasks.createdAt, fromDate) : undefined,
+                //       toDate ? lte(tasks.createdAt, toDate) : undefined,
+                //     );
 
-        const response = await fetch(process.env.API_URL! + "/transactions", {
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            "Authorization": "Bearer " + session?.user.jwt,
-          }
-        })
+                // const orderBy =
+                //   input.sort.length > 0
+                //     ? input.sort.map((item) =>
+                //         item.desc ? desc(tasks[item.id]) : asc(tasks[item.id]),
+                //       )
+                //     : [asc(tasks.createdAt)];
 
-        return await response.json() as TransactionListApiResponse;        
-      } catch {
-        return { data: [], meta: {
-          pagination: {
-            total: 0,
-            page: 1,
-            pageSize: 10,
-            pageCount: 0
-          }
-        } };
-      }
-    },
-    [JSON.stringify(input)],
-    {
-      revalidate: 3600,
-      tags: ["tasks"],
-    },
-  )();
+                const response = await fetch(process.env.API_URL! + '/transactions', {
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                        Authorization: 'Bearer ' + session?.user.jwt,
+                    },
+                });
+
+                return (await response.json()) as TransactionListApiResponse;
+            } catch {
+                return {
+                    data: [],
+                    meta: {
+                        pagination: {
+                            total: 0,
+                            page: 1,
+                            pageSize: 10,
+                            pageCount: 0,
+                        },
+                    },
+                };
+            }
+        },
+        [JSON.stringify(input)],
+        {
+            revalidate: 3600,
+            tags: ['tasks'],
+        }
+    )();
 }
--- apps/epay-admin/app/transactions/_lib/validations.ts
+++ apps/epay-admin/app/transactions/_lib/validations.ts
@@ -1,34 +1,23 @@
-import {
-  createSearchParamsCache,
-  parseAsArrayOf,
-  parseAsInteger,
-  parseAsString,
-  parseAsStringEnum,
-} from "nuqs/server";
-import * as z from "zod";
+import { createSearchParamsCache, parseAsArrayOf, parseAsInteger, parseAsString, parseAsStringEnum } from 'nuqs/server';
+import * as z from 'zod';
 
-import { getFiltersStateParser, getSortingStateParser } from "@/lib/parsers";
-import { Transaction } from "./types";
+import { getFiltersStateParser, getSortingStateParser } from '@/lib/parsers';
+import { Transaction } from './types';
+import { bankOptions } from '@/lib/bank-codes';
 
 export const searchParamsCache = createSearchParamsCache({
-  flags: parseAsArrayOf(z.enum(["advancedTable", "floatingBar"])).withDefault(
-    [],
-  ),
-  page: parseAsInteger.withDefault(1),
-  perPage: parseAsInteger.withDefault(10),
-  sort: getSortingStateParser<Transaction>().withDefault([
-    { id: "createdAt", desc: true },
-  ]),
-  title: parseAsString.withDefault(""),
-  status: parseAsArrayOf(z.enum(['opt1', 'opt2'])).withDefault([]),
-  priority: parseAsArrayOf(z.enum(['opt1', 'opt2'])).withDefault([]),
-  from: parseAsString.withDefault(""),
-  to: parseAsString.withDefault(""),
-  // advanced filter
-  filters: getFiltersStateParser().withDefault([]),
-  joinOperator: parseAsStringEnum(["and", "or"]).withDefault("and"),
+    flags: parseAsArrayOf(z.enum(['advancedTable', 'floatingBar'])).withDefault([]),
+    page: parseAsInteger.withDefault(1),
+    perPage: parseAsInteger.withDefault(10),
+    sort: getSortingStateParser<Transaction>().withDefault([{ id: 'createdAt', desc: true }]),
+    title: parseAsString.withDefault(''),
+    debitor_code: parseAsArrayOf(z.enum(bankOptions.map((otp) => otp.value) as [string, ...string[]])).withDefault([]),
+    creditor_code: parseAsArrayOf(z.enum(bankOptions.map((otp) => otp.value) as [string, ...string[]])).withDefault([]),
+    from: parseAsString.withDefault(''),
+    to: parseAsString.withDefault(''),
+    // advanced filter
+    filters: getFiltersStateParser().withDefault([]),
+    joinOperator: parseAsStringEnum(['and', 'or']).withDefault('and'),
 });
 
-export type GetTransactionsParams = Awaited<
-  ReturnType<typeof searchParamsCache.parse>
->;
+export type GetTransactionsParams = Awaited<ReturnType<typeof searchParamsCache.parse>>;
